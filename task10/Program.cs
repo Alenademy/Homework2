081c1494adc927c1420eeb84fe614ae4a0e337fb
Alena <alenademy7@gmail.com>
2022-12-05 16:45:05 +0500

    Решение задачи 10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+   if (n < 100)
+Console.WriteLine(("Не трехзначное число"));
+else 
+{
+   Console.WriteLine((n / 10) % 10);
+}
+
